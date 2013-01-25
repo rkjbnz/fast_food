6cd5ab8650e386ae0fc890f8501f371c50789023
Richard Biffin <rkjb@me.com>
2013-01-25 15:08:59 +1300

    added in passenger mod recipe

--- chef/cookbooks/finalize/recipes/default.rb
+++ chef/cookbooks/finalize/recipes/default.rb
@@ -3,6 +3,7 @@ include_recipe "imagemagick::devel"
 include_recipe "database::mysql"
 include_recipe "git"
 include_recipe "passenger_apache2"
+include_recipe "passenger_apache2::mod_rails"
 
 # create a mysql database
 mysql_database node['config']['database'] do
